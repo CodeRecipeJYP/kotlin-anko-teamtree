3f85d03ed74aaf54be6323227c1786ec0a03ff89
pjo901018 <pjo901018@korea.ac.kr>
2018-01-25 21:31:47 +0900

    Make UI without xml

--- app/src/main/java/com/asuscomm/yangyinetwork/teamtree_kotlinanko/MainActivity.kt
+++ app/src/main/java/com/asuscomm/yangyinetwork/teamtree_kotlinanko/MainActivity.kt
@@ -2,6 +2,10 @@ package com.asuscomm.yangyinetwork.teamtree_kotlinanko
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.button
+import org.jetbrains.anko.onClick
+import org.jetbrains.anko.relativeLayout
+import org.jetbrains.anko.textView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         var counter = 0
+        relativeLayout {
+            val tv_counter = textView {
+                text = "0"
+                // Float -> SP unit
+                textSize = 24f
+            }
+            button {
+                onClick {
+                    counter++
+                    tv_counter.text = counter.toString()
+                }
+            }
+        }
     }
 }
