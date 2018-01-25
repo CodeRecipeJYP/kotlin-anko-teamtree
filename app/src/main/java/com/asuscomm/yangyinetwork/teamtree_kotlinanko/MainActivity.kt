758949796d8ab7a912fbdb93453b961d5462bd79
pjo901018 <pjo901018@korea.ac.kr>
2018-01-25 21:28:39 +0900

    Remove xml, kotlin-android-extensions plugin

--- app/src/main/java/com/asuscomm/yangyinetwork/teamtree_kotlinanko/MainActivity.kt
+++ app/src/main/java/com/asuscomm/yangyinetwork/teamtree_kotlinanko/MainActivity.kt
@@ -2,18 +2,12 @@ package com.asuscomm.yangyinetwork.teamtree_kotlinanko
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         var counter = 0
-        btn_counter.setOnClickListener {
-            counter++
-            tv_counter.text = counter.toString()
-        }
     }
 }
