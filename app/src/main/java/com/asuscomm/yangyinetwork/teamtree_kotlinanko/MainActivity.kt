5993e120f49aea610d07e824c9d979b893fe4944
pjo901018 <pjo901018@korea.ac.kr>
2018-01-25 21:24:17 +0900

    Use kotlin-android-extensions plugin

--- app/src/main/java/com/asuscomm/yangyinetwork/teamtree_kotlinanko/MainActivity.kt
+++ app/src/main/java/com/asuscomm/yangyinetwork/teamtree_kotlinanko/MainActivity.kt
@@ -2,11 +2,18 @@ package com.asuscomm.yangyinetwork.teamtree_kotlinanko
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var counter = 0
+        btn_counter.setOnClickListener {
+            counter++
+            tv_counter.text = counter.toString()
+        }
     }
 }
